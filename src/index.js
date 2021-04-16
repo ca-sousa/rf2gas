1326d8aa1fcc2265336bc3d18ba3fb4a493a8894
ca-sousa <sousa.ca09@gmail.com>
2021-04-16 15:15:13 -0300

    Config index.js and global.scss

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './assets/global.scss';
 import reportWebVitals from './reportWebVitals';
+
+import './assets/global.scss';
+import 'bootstrap/dist/css/bootstrap.min.css'
+
+import LandingPage from './views/LandingPage';
 
 ReactDOM.render(
   <React.StrictMode>
+    <LandingPage />
   </React.StrictMode>,
   document.getElementById('root')
 );
