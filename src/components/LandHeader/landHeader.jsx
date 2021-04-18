4cf4a0548c6ecc20c016f94b1e9352081486cd42
ca-sousa <sousa.ca09@gmail.com>
2021-04-18 16:10:56 -0300

    Make the Land Page

---
+++ src/components/LandHeader/landHeader.jsx
@@ -0,0 +1,21 @@
+import { Container, Button } from "reactstrap";
+
+import "./landHeader.scss"
+
+function LandHeader() {
+  return (
+    <>
+      <Container fluid>
+        <div className="center-text">
+            <h1>Não deixe de cozinhar pela a falta de Gás</h1>
+            <h3>Instalamos fogões, cooktops e fornos</h3>
+            <Button className="btn-know-more" color="neutral" outline>
+              Conheça mais sobre nós
+            </Button>
+        </div>
+      </Container>
+    </>
+  );
+}
+
+export default LandHeader;
--- src/views/LandingPage.jsx
+++ src/views/LandingPage.jsx
@@ -1,17 +1,12 @@
 import React from "react";
+import LandHeader from "../components/LandHeader/landHeader";
 import NavBarLand from "../components/NavbarLand/navbarLand";
 
 function LandingPage() {
-  document.documentElement.classList.remove("nav-open");
-  React.useEffect(() => {
-    document.body.classList.add("profile-page");
-    return function cleanup() {
-      document.body.classList.remove("profile-page");
-    };
-  });
   return (
     <>
       <NavBarLand />
+      <LandHeader />
     </>
   );
 }
