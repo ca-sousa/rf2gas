411310e2f306c74d723732af4591f3a82b661d68
ca-sousa <sousa.ca09@gmail.com>
2021-04-23 17:23:58 -0300

    Adjustments on Navbar

--- src/components/NavbarLand/navbarLand.jsx
+++ src/components/NavbarLand/navbarLand.jsx
@@ -1,7 +1,6 @@
 import React from "react";
 import classnames from "classnames";
 import {
-  Collapse,
   NavbarBrand,
   Navbar,
   NavItem,
@@ -15,20 +14,14 @@ import "./navbar.scss";
 
 function NavbarLand() {
   const [navbarColor, setNavbarColor] = React.useState("navbar-transparent");
-  const [navbarCollapse, setNavbarCollapse] = React.useState(false);
-
-  const toggleNavbarCollapse = () => {
-    setNavbarCollapse(!navbarCollapse);
-    document.documentElement.classList.toggle("nav-open");
-  };
 
   React.useEffect(() => {
     const updateNavbarColor = () => {
       if (
-        document.documentElement.scrollTop > 299 ||
-        document.body.scrollTop > 299
+        document.documentElement.scrollTop > 850 ||
+        document.body.scrollTop > 850
       ) {
-        setNavbarColor("");
+        setNavbarColor("navbar-color");
       } else if (
         document.documentElement.scrollTop < 300 ||
         document.body.scrollTop < 300
@@ -46,30 +39,16 @@ function NavbarLand() {
   return (
     <Navbar
       className={classnames("fixed-top", navbarColor)}
-      color-on-scroll="300"
       expand="lg"
       style={{width:"95vw"}}
     >
       <Container>
-          <NavbarBrand href="#landpage">
+          <NavbarBrand
+            data-placement="bottom"
+            href="#landpage"
+          >
             <img src={Logo} style={{width:"15vw"}} alt="rf2gas"/> 
           </NavbarBrand>
-          <button
-            aria-expanded={navbarCollapse}
-            className={classnames("navbar-toggler navbar-toggler", {
-              toggled: navbarCollapse,
-            })}
-            onClick={toggleNavbarCollapse}
-          >
-            <span className="navbar-toggler-bar bar1" />
-            <span className="navbar-toggler-bar bar2" />
-            <span className="navbar-toggler-bar bar3" />
-          </button>
-        <Collapse
-          className="justify-content-end"
-          navbar
-          isOpen={navbarCollapse}
-        >
           <Nav className="nav-a">
             <NavItem>
               <NavLink href="#services">
@@ -83,6 +62,7 @@ function NavbarLand() {
             </NavItem>
             <NavItem>
               <NavLink
+                data-placement="bottom"
                 href="https://api.whatsapp.com/send?phone=5511982619325"
                 target="_blank"
                 title="Faça seu orçamento conosco"
@@ -91,7 +71,6 @@ function NavbarLand() {
               </NavLink>
             </NavItem>
           </Nav>
-        </Collapse>
       </Container>
     </Navbar>
   );
