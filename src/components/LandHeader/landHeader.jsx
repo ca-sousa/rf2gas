0cab4b6578848e7e0bd2ad1afa6f6fd5376501ea
ca-sousa <sousa.ca09@gmail.com>
2021-04-23 17:33:47 -0300

    Finish the project for Desktop

--- src/components/LandHeader/landHeader.jsx
+++ src/components/LandHeader/landHeader.jsx
@@ -9,9 +9,11 @@ function LandHeader() {
         <div className="center-text">
             <h1>Não deixe de cozinhar pela a falta de Gás</h1>
             <h3>Instalamos fogões, cooktops e fornos</h3>
-            <Button className="btn-know-more" color="neutral" outline>
-              Conheça mais sobre nós
-            </Button>
+            <a href="#services">
+              <Button className="btn-know-more" outline>
+                Conheça mais sobre nós
+              </Button>
+            </a>
         </div>
       </Container>
     </div>
